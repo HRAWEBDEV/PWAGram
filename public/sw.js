b56e9586fe203bf18920ffc412049a824b7958e7
hamidwise1 <hrawebdev@gmail.com>
2024-03-15 10:27:26 +0330

    add fetch event listener to service worker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
 // there is no dom access in service worker
+// life cycle service worker events
 self.addEventListener('install', (e) => {
  console.log('(from service worker) service worker is installed', e);
 });
@@ -6,4 +7,12 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
  console.log('(from service worker) service worker is activated');
  return self.clients.claim();
+});
+
+// none life cycle service worker events
+self.addEventListener('fetch', (e) => {
+ console.log('(service worker) something is fetched', e);
+ // intercept fetch requests like a proxy
+ // you can override responses here
+ e.respondWith(fetch(e.request));
 });
